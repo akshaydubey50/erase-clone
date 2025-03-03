2e888b3907e31f41492cbdff58541bd09c6b8168
Akshay <akshaydubey2060@gmail.com>
2025-03-03 06:28:49 +0530

    Protected Route Steup

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
+import React from "react";
 
 export default function Home() {
   return (
-    <div>Dashboard</div>
-  )
+    <>
+      <div>Dashboard</div>
+      <Button>
+        <LogoutLink>Logout</LogoutLink>
+      </Button>
+    </>
+  );
 }
