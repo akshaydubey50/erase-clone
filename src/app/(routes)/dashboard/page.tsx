3ac08102f183f23951031bed0aa729061038d371
Akshay <akshaydubey2060@gmail.com>
2025-03-08 21:54:10 +0530

    Ui Component Created

---
+++ src/app/(routes)/dashboard/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  LogoutLink,
+  useKindeBrowserClient,
+} from "@kinde-oss/kinde-auth-nextjs";
+import { useConvex, useMutation, useQuery } from "convex/react";
+import React, { useEffect } from "react";
+import { api } from "../../../../convex/_generated/api";
+import { KindeUser } from "@/types";
+import Header from "./_components/Header";
+import FileList from "./_components/FileList";
+
+export default function Home() {
+  const { user } = useKindeBrowserClient() as { user: KindeUser };
+
+  const createUser = useMutation(api.user.creatUser);
+  const convex = useConvex();
+
+  const checkUser = async () => {
+    const result = await convex.query(api.user.getUser, { email: user?.email });
+    console.log("result:", result);
+    if (!result) {
+      createUser({
+        name: user?.given_name || "",
+        email: user?.email || "",
+        image: user?.picture || "",
+      })
+        .then((res) => console.log("User Created:", res))
+        .catch((err) => console.error("Error creating user:", err));
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      checkUser();
+    }
+  }, [user]);
+
+  return (
+    <>
+       <div className='p-8'>
+      <Header/>
+      <FileList/>
+      {/* <AdBanner
+          data-ad-slot="4796371341"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        /> */}
+    </div>
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ConvexClientProvider } from "./ConvexClientProvider";
+import { Toaster } from "sonner";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <ConvexClientProvider>{children}
+          <Toaster />
+        </ConvexClientProvider>
       </body>
     </html>
   );
