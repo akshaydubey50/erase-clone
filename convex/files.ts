2cfca20b21c036c4a4798e52e4e991674d2a3f3c
Akshay <akshaydubey2060@gmail.com>
2025-03-08 21:54:41 +0530

    config logic handle for db operation

---
+++ convex/files.ts
@@ -0,0 +1,24 @@
+import {v} from "convex/values";
+import {mutation,query} from "./_generated/server"
+
+export const createFile=mutation({
+    args:{
+        fileName:v.string(),
+        teamId:v.string(),
+        createdBy:v.string(),
+        document:v.string(),
+        whiteBoard:v.string(),
+        archive:v.boolean()
+    },
+    handler:async(ctx,args)=>{
+        const result=  await ctx.db.insert('files',args)
+        return result
+    }
+})
+export const getFiles=query({
+    args:{teamId:v.string()},
+    handler:async(ctx,args)=>{
+        const result=await ctx.db.query("files").filter(q=>q.eq(q.field('teamId'),args.teamId)).collect()
+        return result
+    }
+})
